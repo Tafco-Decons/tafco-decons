ed73ef06f9336f679655fd6b3df4cd20d130a524
musharaffalikhan <96779266+musharaffalikhan@users.noreply.github.com>
2023-11-25 18:19:26 +0530

    Added footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "steelblue",
+        color: "skyblue",
+        padding: "3em",
+        width: "100%",
+      }}
+    >
+      Footer
+    </div>
+  );
+};
+
+export default Footer;
