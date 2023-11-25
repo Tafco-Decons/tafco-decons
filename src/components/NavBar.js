7eb87aa2d3471364a1bd9d9d2579a8c2da0d2c35
musharaffalikhan <96779266+musharaffalikhan@users.noreply.github.com>
2023-11-25 18:17:23 +0530

    Added navbar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "steelblue",
+        color: "skyblue",
+        padding: "3em",
+        width: "100%",
+      }}
+    >
+      Header
+    </div>
+  );
+};
+
+export default NavBar;
