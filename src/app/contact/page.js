da0abe91b4306e47715a992c697bc58c6716d545
musharaffalikhan <96779266+musharaffalikhan@users.noreply.github.com>
2023-11-25 20:37:20 +0530

    Added metadata to contact us page

---
+++ src/app/contact/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const metadata = {
+  title: "Contact Us | TAFCO Designs and Constructions",
+};
+
+const Contact = () => {
+  return <div>Contact page</div>;
+};
+
+export default Contact;
