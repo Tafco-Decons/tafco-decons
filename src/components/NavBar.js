ddaeb458fa8d448aff6d2227091bfee594a6bc8b
musharaffalikhan <96779266+musharaffalikhan@users.noreply.github.com>
2023-11-26 01:11:56 +0530

    Modified navbar and added styling to the navbar using navStyles.css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,30 +1,44 @@
+"use client";
+import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
+import "@/components/NavStyles.css";
 
 const NavBar = () => {
+  const pathname = usePathname();
   const navLinks = [
-    { name: "Home", href: "/" },
+    { name: "Home", href: "/home" },
     { name: "About", href: "/about" },
     { name: "Services", href: "/services" },
     { name: "Projects", href: "/projects" },
     { name: "Contact", href: "/contact" },
   ];
   return (
-    <div
-      style={{
-        backgroundColor: "steelblue",
-        color: "skyblue",
-        padding: "3em",
-        width: "100%",
-      }}
-    >
-      {navLinks.map((link) => {
-        return (
-          <Link href={link.href} key={link.name}>
-            {link.name}
-          </Link>
-        );
-      })}
+    <div class="container">
+      <div class="image">
+        <Image
+          src="/assets/TealLogo.png"
+          width={234}
+          height={120}
+          alt="Screenshots of the dashboard project showing desktop version"
+        />
+      </div>
+
+      <div class="navlinks">
+        {navLinks.map((link) => {
+          const isActive = pathname.startsWith(link.href);
+          return (
+            <Link
+              href={link.href}
+              key={link.name}
+              class={isActive ? "activeLink" : "link"}
+            >
+              {link.name}
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 };
