49a39d775d76ac11a9eb85c74f04589ca9bae4c2
musharaffalikhan <96779266+musharaffalikhan@users.noreply.github.com>
2023-11-25 20:36:52 +0530

    Added metadata to about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
+
+export const metadata = {
+  title: "About | TAFCO Designs and Constructions",
+};
 
 const About = () => {
-  return (
-    <div>About Page</div>
-  )
-}
+  return <div>About Page</div>;
+};
 
-export default About
+export default About;
