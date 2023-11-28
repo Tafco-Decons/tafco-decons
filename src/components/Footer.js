50e3a11674509d85238358475adb8f0a238700be
musharaffalikhan <96779266+musharaffalikhan@users.noreply.github.com>
2023-11-28 17:59:42 +0530

    Added footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,31 @@
 import React from "react";
+import "@/components/FooterStyles.css";
+import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div
-      style={{
-        backgroundColor: "steelblue",
-        color: "skyblue",
-        padding: "3em",
-        width: "100%",
-      }}
-    >
-      Footer
+    <div class="Fcontainer">
+      <div class="Ftitle">
+        <a>TAFCO Designs and Constructions</a>
+      </div>
+      <div class="address">
+        <span>TAFCO Designs and Constructions</span>
+        <br></br>
+        <span>(A division of Entropy Management Services)</span>
+        <br></br>
+        <span>
+          49/1, Anees Plaza, 2nd floor, Rashtriya Vidyalaya Rd, Basavanagudi,
+          Bengaluru, Karnataka-560004
+        </span>
+      </div>
+      <div class="FiconContainer">
+        <Image src="/assets/Facebook.png" width={20} height={20} />
+        <Image src="/assets/Twitter.png" width={20} height={20} />
+        <Image src="/assets/LinkedIn.png" width={20} height={20} />
+      </div>
+      <div class="copyright">
+        <span>©2020 by TAFCO Designs and Constructions.</span>
+      </div>
     </div>
   );
 };
