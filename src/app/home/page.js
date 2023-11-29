bbd2f828c214e845e522ce43739060c16a682337
musharaffalikhan <96779266+musharaffalikhan@users.noreply.github.com>
2023-11-29 23:39:30 +0530

    Added home page

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,11 +1,32 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import "@/app/home/HomeStyles.css";
+import { Button } from "rsuite";
 
 const HomePage = () => {
+  const [isTextVisible, setIsTextVisible] = useState(false);
+
+  useEffect(() => {
+    // Add the 'show' class after a delay (adjust the delay as needed)
+    const timeoutId = setTimeout(() => {
+      setIsTextVisible(true);
+    }, 500);
+
+    // Clear the timeout when the component is unmounted
+    return () => clearTimeout(timeoutId);
+  }, []);
+
   return (
-    <div class="title">
-      <span> TAFCO Designs and Constructions</span>
-      <h6> Our Expertise for Your Project</h6>
+    <div class="HImageContainer">
+      <div class={`title ${isTextVisible ? "show" : ""}`}>
+        <span style={{ marginBottom: "2em" }}>
+          TAFCO Designs and Constructions
+        </span>
+        <h6>OUR EXPERTISE FOR YOUR PROJECT</h6>
+      </div>
+      <Button style={{ color: "white" }} appearance="ghost">
+        LEARN MORE{" "}
+      </Button>
     </div>
   );
 };
