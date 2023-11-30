b7f497395ebaaaf057a481f80bf6011f21ecf1a7
musharaffalikhan <96779266+musharaffalikhan@users.noreply.github.com>
2023-11-30 19:36:31 +0530

    Added LetsChat .js

---
+++ src/components/LetsChat.js
@@ -0,0 +1,39 @@
+"use client";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
+import React, { useState } from "react";
+import { Button, Modal, Placeholder } from "rsuite";
+
+const LetsChat = () => {
+  const [open, setOpen] = useState(false);
+  const isMobile = useMediaQuery("(max-width: 600px)");
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  return (
+    <>
+      <div style={{ position: "fixed", bottom: "20px", right: "20px" }}>
+        <Button appearance="default" onClick={handleOpen}>
+          Let's Chat
+        </Button>
+      </div>
+      <Modal
+        size={isMobile ? "full" : "xs"}
+        open={open}
+        onClose={handleClose}
+        backdrop={true}
+        style={
+          isMobile ? {} : { position: "fixed", bottom: "20px", right: "20px" }
+        }
+      >
+        <Modal.Header>
+          <Modal.Title>Modal Title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Placeholder.Paragraph rows={isMobile ? 100 : 10} />
+        </Modal.Body>
+        <Modal.Footer></Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default LetsChat;
